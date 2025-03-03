7dceef47db4df4bb86e10bd9583ad4ef0bb82bba
1-DARK <harshitgit23@gmail.com>
2025-03-03 14:38:53 +0530

    recent diff 120

--- main.go
+++ main.go
@@ -255,7 +255,7 @@ func processTradeHistory(coin string) {
 		recentDiff := (float64(timeNow)) - (times[0] / 1000)
 		// fmt.Println(diff, recentDiff)
 		// fmt.Printf("Diff: %.2f Recent Diff: %.2f \n", diff, recentDiff)
-		if diff > 60 || recentDiff > 60.0 {
+		if diff > 120 || recentDiff > 60.0 {
 			// highTimeDiff = append(highTimeDiff, diff)
 			return
 		}
